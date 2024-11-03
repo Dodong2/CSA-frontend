460187de8236bcbb170f5f4f1ba9860aec04b214
Dodong2 <carlseighartaliodegmail.com>
2024-11-03 19:39:10 +0800

    latest

--- src/validations/ServiceWorkerValidation.tsx
+++ src/validations/ServiceWorkerValidation.tsx
@@ -1,12 +1,22 @@
   //PWA service worker
+  // if ('serviceWorker' in navigator) {
+  //   window.addEventListener('load', () => {
+  //     navigator.serviceWorker.register('/src/service-worker.js')
+  //       .then((registration) => {
+  //         console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  //       })
+  //       .catch((error) => {
+  //         console.error('ServiceWorker registration failed: ', error);
+  //       });
+  //   });
+  // }
+
   if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/src/service-worker.js')
-        .then((registration) => {
-          console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        })
-        .catch((error) => {
-          console.error('ServiceWorker registration failed: ', error);
-        });
-    });
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed:', error);
+      });
   }
